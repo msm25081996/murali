a7bae0da5dc6afb8f3f87b4369e42f714b696a00
msm25081996 <murali1996.muramalla@gmail.com>
2025-08-01 14:39:11 +0530

    Update file11.py

--- file11.py
+++ file11.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-# This is from the `master` branch
-print("hi")
-list1 = [1, 2, 3, 4]
-list2 = [3, 4, 5, 6]
-
-unique_numbers = list(set(list1) ^ set(list2))
-print(unique_numbers)
-=======
-# This is from the `dumb` branch
 print("hi")
 list1 = [1, 2, 3, 4]
 list2 = [3, 4, 5, 6]
@@ -18,4 +8,3 @@ print(unique_numbers)
 # Add 100 to each unique number
 result = [num + 100 for num in unique_numbers]
 print(result)
->>>>>>> dumb
