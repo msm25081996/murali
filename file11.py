5a4163e1ae55e32f073db21d0076df44dbf959d2
msm25081996 <murali1996.muramalla@gmail.com>
2025-08-01 13:35:29 +0530

    Add files via upload

--- file11.py
+++ file11.py
@@ -4,3 +4,9 @@ list2 = [3, 4, 5, 6]
 
 unique_numbers = list(set(list1) ^ set(list2))
 print(unique_numbers)
+
+# Add 100 to each unique number
+result = [num + 100 for num in unique_numbers]
+print(result)
+
+
